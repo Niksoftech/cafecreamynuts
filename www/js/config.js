2b993ff06b9284b3eeffe1811c23c7d34bb5624a
Nikhil Mane <cafenikhilbypuretech@gmail.com>
2017-03-08 15:20:06 +0530

    Fixed the API keys and build project

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 
 var krms_config ={	
-	'ApiUrl' : "http://www.cafecreamynuts.in/mobileapp/api",
+	'ApiUrl' : "http://cafecreamynuts.in/mobileapp/api",
 	'DialogDefaultTitle' : "Cafe Creamy Nuts",
 	'pushNotificationSenderid' : "AIzaSyBfBM_PCU5Wah5zTN1oSEEj1ROqb5CPbh0 ",
 	'facebookAppId' : "1582771952032659",
-	'APIHasKey' : "ab7235d8ddf7b3dab24b544b3993020e"
+	'APIHasKey' : "33c882ca2b8463d9d61598c8e31d4c0f"
 };
